01fb34720125c647418259b39161409b4434b07f
ImGMin <76603110+ImGMin@users.noreply.github.com>
2021-01-28 18:17:12 +0900

    단서 팝업
    일기, 쪽지, 차트 팝업

---
+++ Team2Project/Clues/Assets/ClickNext.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ClickNext : MonoBehaviour
+{
+    public Image []_UI_Charts;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    public void OnMouseUp()
+    {
+        _UI_Charts[2].gameObject.SetActive(true);
+        _UI_Charts[1].gameObject.SetActive(true);
+        _UI_Charts[0].gameObject.SetActive(false);
+        _UI_Charts[3].gameObject.SetActive(false);
+
+    }
+}
---
+++ Team2Project/Clues/Assets/TouchClipboard.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TouchClipboard : MonoBehaviour
+{
+    public Image _UI_ChartA;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnMouseDown()
+    {
+    }
+
+    private void OnMouseUp()
+    {
+        _UI_ChartA.gameObject.SetActive(true);
+
+    }
+
+}
